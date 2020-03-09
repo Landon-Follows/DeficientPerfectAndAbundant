6dee076cfe57dd5d06cc5e94115c9337b33dcbee
Wellesley Yu <byu@lcs.on.ca>
2020-03-09 11:12:37 -0400

    Initial Commit

---
+++ DeficientPerfectAndAbundant/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DeficientPerfectAndAbundant
+//
+//  Created by Yu, Boning on 2020-03-09.
+//  Copyright © 2020 Wellesley Yu. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
